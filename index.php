910689419e6ad9078562a744d5406d2972db63c6
Your NameZenilton <ze18.fv@gmail.com>
2022-09-10 16:57:56 -0300

    icon ao lado da index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>painel RH</title>
+    <link rel="stylesheet" href="/css/style.css">
+    
+</head>
+<body>
+
+   <div  id="container">
+      <div id="icon">
+         <div id="teste">
+            <div  id="foto">
+            </div>
+
+         </div>
+        
+         <div id="texto">
+            <p>Boa tarde, tenha um ótimo trabalho ;)</p>
+         </div>
+      </div>
+      <div id="formulario">
+         <form action="">
+            <div>
+               <input type="text" placeholder="CPF">
+            </div>
+            <div>
+               <input type="password" name="" id="" placeholder="SENHA">
+            </div>
+            <div >
+               <input class="button" type="submit" value="Entrar">
+            </div>
+         </form>
+      </div>
+      
+    
+   </div>
+
+   <div id="container2">
+      <div id="lado1">
+
+     </div>
+     <div class="body">
+      
+
+     </div>
+   </div>
+    
+</body>
+</html>
